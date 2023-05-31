c0f17ee8c5dc2def96663a2867f41a51a7418757
debesis27 <shivam27.maske@gmail.com>
2023-05-31 17:52:57 +0530

    Added Navigation between different screens with NavController

--- app/src/main/java/com/example/dishpedia/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/dishpedia/ui/screens/HomeScreen.kt
@@ -3,17 +3,30 @@ package com.example.dishpedia.ui.screens
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,17 +39,52 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.dishpedia.DishpediaScreen
 import com.example.dishpedia.R
+import com.example.dishpedia.models.NavigationDrawerItems
 import com.example.dishpedia.models.Recipe
 import com.example.dishpedia.models.Recipes
+import com.example.dishpedia.ui.theme.Purple500
 import com.example.dishpedia.viewmodel.RecipesUiState
 import com.example.dishpedia.viewmodel.RecipesViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
-fun HomeScreen(recipesViewModel: RecipesViewModel){
+fun HomeScreen(
+    recipesViewModel: RecipesViewModel,
+    navController: NavController
+){
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    val navItems = listOf(
+        NavigationDrawerItems(
+            name = "Home",
+            route = "Home",
+        ),
+        NavigationDrawerItems(
+            name = "My Recipes",
+            route = "MyRecipes",
+        ),
+        NavigationDrawerItems(
+            name = "Account",
+            route = "Account",
+        )
+    )
+
     Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { HomeScreenAppBar(coroutineScope = coroutineScope, scaffoldState = scaffoldState, navController = navController) },
+        drawerContent = { HomeScreenDrawer(navItems, navController, coroutineScope, scaffoldState) },
+        drawerGesturesEnabled = true,
         content = {
             Column(modifier = Modifier) {
                 Row(
@@ -189,7 +237,10 @@ fun Carousel(
 }
 
 @Composable
-fun RecipePhotoCard(recipe: Recipe, modifier: Modifier = Modifier){
+fun RecipePhotoCard(
+    recipe: Recipe,
+    modifier: Modifier = Modifier
+){
     Card(
         modifier = modifier
             .padding(4.dp)
@@ -226,6 +277,98 @@ fun RecipeStaggeredGrid(
     ){
         items(items){
             recipe -> RecipePhotoCard(recipe = recipe)
+        }
+    }
+}
+
+@Composable
+fun HomeScreenAppBar(
+    modifier: Modifier = Modifier,
+    coroutineScope: CoroutineScope,
+    scaffoldState: ScaffoldState,
+    navController: NavController
+){
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name)) },
+        modifier = modifier,
+        navigationIcon = {
+                IconButton(
+                    onClick = { coroutineScope.launch { scaffoldState.drawerState.open() } }
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Menu,
+                        contentDescription = stringResource(R.string.menu_button)
+                    )
+                }
+        },
+        actions = {
+            IconButton(onClick = { navController.navigate(DishpediaScreen.Search.name) }) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = stringResource(id = R.string.search_button),
+                    tint = Color.White
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeScreenDrawer(
+    navItems: List<NavigationDrawerItems>,
+    navController: NavController,
+    coroutineScope: CoroutineScope,
+    scaffoldState: ScaffoldState
+){
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 8.dp, top = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(126.dp)
+                .clip(CircleShape),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.guest_icon),
+                contentDescription = null,
+                modifier = Modifier.matchParentSize()
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        navItems.forEach{ item ->
+            val selected = item.route == backStackEntry?.destination?.route
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .clickable {
+                        coroutineScope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                        navController.navigate(item.route)
+                    },
+                backgroundColor = if (selected) Purple500 else Color.White,
+                elevation = 0.dp,
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = item.name,
+                        modifier = Modifier.padding(start = 24.dp)
+                    )
+                }
+            }
         }
     }
 }
